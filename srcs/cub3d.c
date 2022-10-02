434018f3df15769619eb222a262bf40f3b7660c0
Daniele Ridolfo <dridolfo@e4r6p2.42roma.it>
2022-10-02 12:38:44 +0200

    mistakes were made, now the casting is really working

--- srcs/cub3d.c
+++ srcs/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: dridolfo <dridolfo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 12:13:59 by dridolfo          #+#    #+#             */
-/*   Updated: 2022/10/01 19:08:46 by dridolfo         ###   ########.fr       */
+/*   Updated: 2022/10/02 12:37:48 by dridolfo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,25 +27,22 @@ int end_game(t_game *game, int arg)
 	exit(0);
 }
 
+void	_init_directions(t_game *game, t_state *state)
+{
+	state->posx = 22;
+	state->posy = 22;
+	state->dirx = -1;
+	state->diry = 0;
+	state->angle[0] = 0;
+	state->angle[1] = 0.66;
+	game->state = state;
+}
+
 void	_init(t_game *game)
 {
-	t_state state;
-
 	game->mlx = mlx_init();
 	game->mlx_win = mlx_new_window(game->mlx, WINDOW_W, WINDOW_H, "GcubeDanDanDan");
 	game->map = map_init("map.gcube");
-	ft_memset(&state, 0, sizeof(t_state));
-	state.posx = 12;
-	state.posy = 12;
-	state.dirx = -1;
-	state.diry = 0;
-	state.angle[0] = 0;
-	state.angle[1] = 0.66;
-	game->state = &state;
-	//init_directions(game, 'N');
-	printf("EMILY %lf %lf %lf %lf %lf %lf %lf\n", game->state->posx, game->state->posy,
-	game->state->dirx, game->state->diry, game->state->angle[0], game->state->angle[1],
-	game->state->camx);
 }
 
 int	check_cond(t_game *game, int x, int y)
@@ -66,50 +63,33 @@ int	key_filter(int keycode, t_game *game)
 	int	i;
 
 	i = 0;
-	printf("EMILY %lf %lf %lf %lf %lf %lf %lf\n", game->state->posx, game->state->posy,
-	game->state->dirx, game->state->diry, game->state->angle[0], game->state->angle[1],
-	game->state->camx);
 	if (keycode == 53)
 		end_game(game, 0);
 	else if (keycode == 13 || keycode == 126)
 	{
 		if (!check_cond(game, 0, -1))
 			move(game, 0, -1);
-		else
-			end_game(game, 0);
 	}
 	else if (keycode == 0 || keycode == 123)
 	{
 		if (!check_cond(game, -1, 0))
 			move(game, -1, 0);
-		else
-			end_game(game, 0);
 	}
 	else if (keycode == 1 || keycode == 125)
 	{
 		if (!check_cond(game, 0, 1))
 			move(game, 0, 1);
-		else
-			end_game(game, 0);
 	}
 	else if (keycode == 2 || keycode == 124)
 	{
 		if (!check_cond(game, 0, -1))
-			move(game, 0, -1);
-		else
-			end_game(game, 0);
+			move(game, 1, 0);
 	}
-	printf("CULO %lf %lf %lf %lf %lf %lf %lf\n", game->state->posx, game->state->posy,
-		game->state->dirx, game->state->diry, game->state->angle[0], game->state->angle[1],
-		game->state->camx);
 	return (0);
 }
 
 int	game_loop(t_game *game)
 {
-	printf("CULO %lf %lf %lf %lf %lf %lf %lf\n", game->state->posx, game->state->posy,
-		game->state->dirx, game->state->diry, game->state->angle[0], game->state->angle[1],
-		game->state->camx);
 	raycast(game);
 	return (0);
 }
@@ -117,11 +97,10 @@ int	game_loop(t_game *game)
 int main(void)
 {
 	t_game	game;
+	t_state	state;
 
 	_init(&game);
-	printf("EMILY %lf %lf %lf %lf %lf %lf %lf\n", game.state->posx, game.state->posy,
-	game.state->dirx, game.state->diry, game.state->angle[0], game.state->angle[1],
-	game.state->camx);
+	_init_directions(&game, &state);
 	mlx_hook(game.mlx_win, 17, 0, end_game, &game);
 	mlx_loop_hook(game.mlx, game_loop, &game);
 	mlx_key_hook(game.mlx_win, key_filter, &game);
--- srcs/engine/draw.c
+++ srcs/engine/draw.c
@@ -6,7 +6,7 @@
 /*   By: dridolfo <dridolfo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 12:31:27 by dridolfo          #+#    #+#             */
-/*   Updated: 2022/10/01 16:46:14 by dridolfo         ###   ########.fr       */
+/*   Updated: 2022/10/02 12:34:33 by dridolfo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,12 @@ void	draw_ray(t_game *game, int x, int y, t_img *img)
 		my_mlx_pixel_put(img, x, v++, 0x000089AD);
 	v = y;
 	while (v < game->ray->draw[2])
-		my_mlx_pixel_put(img, x, v++, 0x0000FF00);
+	{
+		if (game->ray->side == 1)
+			my_mlx_pixel_put(img, x, v++, 0x0040c600 / 2);
+		else
+			my_mlx_pixel_put(img, x, v++, 0x0040c600);
+	}
 	while (v < WINDOW_H)
 		my_mlx_pixel_put(img, x, v++, 0x00403125);
 }
