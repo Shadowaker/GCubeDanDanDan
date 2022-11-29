81a13aa5452a06c576aca690824f994fb92a32ce
Daniele Ridolfo <shadan@Shadan.localdomain>
2022-11-29 21:06:49 +0100

    Init fill algorithm

--- srcs/utils/mapvalidator.c
+++ srcs/utils/mapvalidator.c
@@ -12,71 +12,33 @@
 
 #include "../../incl/gcube.h"
 
-int	first_recurrence(char *str, char c)
+void	find_char(char **map, char c, int *ids)
 {
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0' && str != NULL)
+	ids[1] = 0;
+	while (map[ids[1]] != NULL)
 	{
-		if (c == str[i])
-			return (i);
-		i++;
+		ids[0] = 0;
+		while (map[ids[1]][ids[0]] != '\0')
+		{
+			if (map[ids[1]][ids[0]] == c)
+				return ;
+			ids[0]++;
+		}
+		ids[1]++;
 	}
-	return (-1);
+	ids[1] = -1;
+	ids[0] = -1;
 }
 
-int last_recurrence(char *str, char c)
+int	fill_algorithm(char **map)
 {
-	int	i;
+	int	pl[2];
 
-	i = ft_strlen(str);
-	while (i >= 0 && str != NULL)
-	{
-		if (c == str[i])
-			return (i);
-		i--;
-	}
-	return (-1);
+	find_char(map, '1', pl);
 }
 
-int first_to_last(char *str, int *left, int *right)
-{
-	left[1] = first_recurrence(str, '1');
-	if (left[1] == -1)
-		return (-1);
-	while (str[++left[1]] != '\0' && str[left[1]] == '1')
-		;
-	--left[1];
-	right[1] = last_recurrence(str, '1');
-	if (right[1] == -1)
-		return (-1);
-	while (++right[1] >= *left && str[right[1]] == '1')
-		;
-	--right[1];
-	return (0);
-}
-
-int	next_to(char *str, int start, int end, char c)
-{
-	int	i;
-
-	i = start;
-	while (str[i] != '\0' && str[i] != c)
-		i++;
-	if (i != end && str[i] != '\0')
-		return (1);
-	return (0);
-}
 
 int	map_validator(char **map)
 {
-	int	i;
-
-	i = 0;
-	while (map[i] != NULL)
-	{
-
-	}
-	return (0);
+	;
 }
